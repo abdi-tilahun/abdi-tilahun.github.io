e21781aa1d958432e7703f00e56880164e93d9b1
abdi-tilahun <abditilahun64@gmail.com>
2026-07-07 12:50:04 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 window.onload = function () {
     const themeToggle = document.getElementById("themeToggle");
 
+    if (localStorage.getItem("theme") === "light") {
+        document.body.classList.add("light-mode");
+        themeToggle.textContent = "☀️";
+    }
+
     themeToggle.onclick = function () {
         document.body.classList.toggle("light-mode");
 
         if (document.body.classList.contains("light-mode")) {
             themeToggle.textContent = "☀️";
+            localStorage.setItem("theme", "light");
         } else {
             themeToggle.textContent = "🌙";
+            localStorage.setItem("theme", "dark");
         }
     };
 };
